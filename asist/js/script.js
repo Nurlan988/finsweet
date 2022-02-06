3ba62a4abd29e4cf40ad7476dfb85110195129d6
Nurlan988 <98158913+Nurlan988@users.noreply.github.com>
2022-02-06 15:58:34 +0400

    home site

--- asist/js/script.js
+++ asist/js/script.js
@@ -10,3 +10,37 @@ $('.owl-carousel').owlCarousel({
     touchDrag: true,
     items:1
 })
+
+//NAVBAR
+
+const burger = document.querySelector('.trigger-menu');
+const closeCurcor = document.querySelector('.close-menu');
+const subMenu = document.querySelector('.new-sub-menu');
+
+function openMenu() {
+	burger.style.display = "none";
+	closeCurcor.style.display = "block";
+	subMenu.style.display = "block";
+}
+
+function closeMenu() {
+	burger.style.display = "block";
+	closeCurcor.style.display = "none";
+	subMenu.style.display = "none";
+}
+
+const subMenuLink = document.querySelectorAll('a.new-nav-link');
+subMenuLink.forEach(event => {
+	event.addEventListener('click', closeMenu)
+})
+
+//FAQ TOGGLE
+
+const faqQuestions = document.querySelectorAll('.faq-questions');
+
+faqQuestions.forEach(toggleEvent => {
+	toggleEvent.addEventListener('click', () =>{
+		faqQuestions.forEach(event => event.classList.remove('active'));
+		toggleEvent.classList.add('active');
+	})
+})
