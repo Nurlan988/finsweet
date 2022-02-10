c52251e174b78f05631f1b7253f98f4004b5a0db
Nurlan998 <aliyev.g.nurlan@gmail.com>
2022-02-10 19:20:49 +0400

    appear-effect

--- asist/js/script.js
+++ asist/js/script.js
@@ -1,6 +1,6 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:true,
+    loop: true,
+    nav: true,
     navText: [
         '<i class="fa fa-angle-left fa-2x"></i>',
         '<i class="fa fa-angle-right fa-2x"></i>'
@@ -8,7 +8,7 @@ $('.owl-carousel').owlCarousel({
     dots: false,
     mouseDrag: false,
     touchDrag: true,
-    items:1
+    items: 1
 })
 
 //NAVBAR
@@ -18,20 +18,20 @@ const closeCurcor = document.querySelector('.close-menu');
 const subMenu = document.querySelector('.new-sub-menu');
 
 function openMenu() {
-	burger.style.display = "none";
-	closeCurcor.style.display = "block";
-	subMenu.style.display = "block";
+    burger.style.display = "none";
+    closeCurcor.style.display = "block";
+    subMenu.style.display = "block";
 }
 
 function closeMenu() {
-	burger.style.display = "block";
-	closeCurcor.style.display = "none";
-	subMenu.style.display = "none";
+    burger.style.display = "block";
+    closeCurcor.style.display = "none";
+    subMenu.style.display = "none";
 }
 
 const subMenuLink = document.querySelectorAll('a.new-nav-link');
 subMenuLink.forEach(event => {
-	event.addEventListener('click', closeMenu)
+    event.addEventListener('click', closeMenu)
 })
 
 //FAQ TOGGLE
@@ -39,7 +39,50 @@ subMenuLink.forEach(event => {
 const faqQuestions = document.querySelectorAll('.faq-questions');
 
 faqQuestions.forEach(toggleEvent => {
-	toggleEvent.addEventListener('click', () =>{
-		toggleEvent.classList.toggle('active');
-	})
+    toggleEvent.addEventListener('click', () => {
+        toggleEvent.classList.toggle('active');
+    })
 })
+
+//APPEAR EFFECT
+
+const postContent = document.querySelectorAll('.post-content');
+const windowHeight = window.innerHeight*1.5;
+
+const postLeftContentImage = document.querySelectorAll('.post-left .post-image');
+const postRightContentImage = document.querySelectorAll('.post-right .post-image');
+
+window.addEventListener('scroll',showAppearEffect);
+
+function showAppearEffect() {
+    for (var i = 0; i < postContent.length; i++){
+        const postContentTop = postContent[i].getBoundingClientRect().top;
+        if (postContentTop < windowHeight/1.3) {
+            postContent[i].classList.add('post-content-appear');
+        } else{
+            postContent[i].classList.remove('post-content-appear');
+        }
+    }
+    // for(var i = 0; i < postLeftContentImage.length; i++){
+    //     const postLeftContentImageLeft = postLeftContentImage[i].getBoundingClientRect().left - windowHeight*1.1;
+    //     console.log(postLeftContentImageLeft);
+    //     if (postLeftContentImageLeft < 0) {
+    //         postLeftContentImage[i].classList.add('post-image-left-appear');
+    //     }else{
+    //         postLeftContentImage[i].classList.remove('post-image-left-appear');
+    //     }
+    // }
+    // for(var i = 0; i < postRightContentImage.length; i++){
+    //     const postRightContentImageRight = postRightContentImage[i].getBoundingClientRect().left - windowHeight*2;
+    //     console.log(postRightContentImageRight);
+    //     if (postRightContentImageRight < 0) {
+    //         postRightContentImage[i].classList.add('post-image-left-appear');
+    //     }else{
+    //         postRightContentImage[i].classList.remove('post-image-left-appear');
+    //     }
+    // }
+};
+showAppearEffect()
+
+
+
