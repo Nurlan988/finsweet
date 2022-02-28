d0a613bc6cd4f4629887fba90405e37401a030f8
Nurlan998 <aliyev.g.nurlan@gmail.com>
2022-02-28 11:57:06 +0400

    post-right-row

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,24 +51,24 @@ const windowHeight = window.innerHeight;
 const postLeftContentImage = document.querySelectorAll('.post-left .post-image');
 const postRightContentImage = document.querySelectorAll('.post-right .post-image');
 
-window.addEventListener('scroll',showAppearEffect);
+window.addEventListener('scroll', showAppearEffect);
 
 function showAppearEffect() {
-    for (var i = 0; i < postContent.length; i++){
+    for (var i = 0; i < postContent.length; i++) {
         const postContentTop = postContent[i].getBoundingClientRect().top;
-        if (postContentTop < windowHeight/1.2) {
+        if (postContentTop < windowHeight / 1.2) {
             postContent[i].classList.add('post-content-appear');
         }
     }
-    for(var i = 0; i < postLeftContentImage.length; i++){
+    for (var i = 0; i < postLeftContentImage.length; i++) {
         const postLeftContentImageLeft = postLeftContentImage[i].getBoundingClientRect().top;
-        if (postLeftContentImageLeft < windowHeight/1) {
+        if (postLeftContentImageLeft < windowHeight / 1) {
             postLeftContentImage[i].classList.add('post-image-left-appear');
         }
     }
-    for(var i = 0; i < postRightContentImage.length; i++){
+    for (var i = 0; i < postRightContentImage.length; i++) {
         const postRightContentImageRight = postRightContentImage[i].getBoundingClientRect().top;
-        if (postRightContentImageRight < windowHeight/1) {
+        if (postRightContentImageRight < windowHeight / 1) {
             postRightContentImage[i].classList.add('post-image-right-appear');
         }
     }
@@ -77,17 +77,17 @@ function showAppearEffect() {
 //PORTFOLIO NAV
 
 const portfolioNavList = document.querySelectorAll('.portfolio-nav ul li a');
-portfolioNavList.forEach(navList_element =>{
-    navList_element.addEventListener("click", () =>{
+portfolioNavList.forEach(navList_element => {
+    navList_element.addEventListener("click", () => {
         portfolioNavList.forEach(list_element => list_element.classList.remove('active'));
         navList_element.classList.add('active');
     })
 })
 
 const portfolioBox = document.querySelectorAll('.portfolio-box');
-portfolioNavList.forEach(navList_element =>{
-    navList_element.addEventListener("click", () =>{
-        for(let i = 0; i < portfolioBox.length; i++){
+portfolioNavList.forEach(navList_element => {
+    navList_element.addEventListener("click", () => {
+        for (let i = 0; i < portfolioBox.length; i++) {
             portfolioBox[i].classList.remove('active');
         }
         document.querySelector(navList_element.getAttribute('data-target')).classList.add('active');
@@ -100,7 +100,12 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('.nav-menu ul.nav a.nav-link');
 
 navLinks.forEach(link => {
-    if(link.href.includes(`${activePage}`)){
+    if (link.href.includes(`${activePage}`)) {
         link.classList.add('active');
     }
 })
+
+function myFunction (a) {
+    return Math.max.apply(null, a);
+}
+myFunction(1,2,3);
